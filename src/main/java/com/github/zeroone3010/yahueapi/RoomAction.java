447d68d890080f40c7982f5d7ad82b3ae5d6c211
Ville Saalo <ville.saalo@iki.fi>
2018-07-20 23:01:39 +0300

    Refactor: extract method

--- src/main/java/com/github/zeroone3010/yahueapi/RoomAction.java
+++ src/main/java/com/github/zeroone3010/yahueapi/RoomAction.java
@@ -64,15 +64,19 @@ public final class RoomAction {
     final float red = color.getRed() / 255f;
     final float green = color.getGreen() / 255f;
     final float blue = color.getBlue() / 255f;
-    final double r = (red > 0.04045f) ? Math.pow((red + 0.055f) / (1.0f + 0.055f), 2.4f) : (red / 12.92f);
-    final double g = (green > 0.04045f) ? Math.pow((green + 0.055f) / (1.0f + 0.055f), 2.4f) : (green / 12.92f);
-    final double b = (blue > 0.04045f) ? Math.pow((blue + 0.055f) / (1.0f + 0.055f), 2.4f) : (blue / 12.92f);
+    final double r = gammaCorrection(red);
+    final double g = gammaCorrection(green);
+    final double b = gammaCorrection(blue);
     final double rgbX = r * 0.664511f + g * 0.154324f + b * 0.162028f;
     final double rgbY = r * 0.283881f + g * 0.668433f + b * 0.047685f;
     final double rgbZ = r * 0.000088f + g * 0.072310f + b * 0.986039f;
     final float x = (float) (rgbX / (rgbX + rgbY + rgbZ));
     final float y = (float) (rgbY / (rgbX + rgbY + rgbZ));
     return new XAndYAndBrightness(x, y, (int) (rgbY * 255f));
+  }
+
+  private static double gammaCorrection(float component) {
+    return (component > 0.04045f) ? Math.pow((component + 0.055f) / (1.0f + 0.055f), 2.4f) : (component / 12.92f);
   }
 
   private static class XAndYAndBrightness {
