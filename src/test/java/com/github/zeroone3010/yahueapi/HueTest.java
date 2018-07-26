f690eabe71f4037031b64e8e6b556ec3efb5f59a
Ville Saalo <ville.saalo@iki.fi>
2018-07-26 23:42:20 +0300

    Add one trivial WireMock integration test

---
+++ src/test/java/com/github/zeroone3010/yahueapi/HueTest.java
@@ -0,0 +1,52 @@
+package com.github.zeroone3010.yahueapi;
+
+import com.github.jknack.handlebars.internal.Files;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+class HueTest {
+  private static final String API_KEY = "abcd1234";
+
+  final WireMockServer wireMockServer = new WireMockServer(wireMockConfig().dynamicPort());
+
+  @BeforeEach
+  void startServer() {
+    wireMockServer.start();
+  }
+
+  @AfterEach
+  void stopServer() {
+    wireMockServer.stop();
+  }
+
+  @Test
+  void testInitialLoading() throws IOException {
+    final String read = readFile("hueRoot.json");
+    wireMockServer.stubFor(get(urlEqualTo("/api/" + API_KEY + "/"))
+        .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", "application/json")
+            .withBody(read)));
+
+    new Hue("localhost:" + wireMockServer.port(), API_KEY);
+
+    wireMockServer.verify(getRequestedFor(urlEqualTo("/api/" + API_KEY + "/")));
+  }
+
+  private String readFile(final String fileName) throws IOException {
+    final ClassLoader classLoader = getClass().getClassLoader();
+    final File file = new File(classLoader.getResource(fileName).getFile());
+    return Files.read(file);
+  }
+}
