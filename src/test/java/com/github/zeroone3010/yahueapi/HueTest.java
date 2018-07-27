c99a77dbd24e2b6998c6c3e4533a9d07804b0265
Ville Saalo <ville.saalo@iki.fi>
2018-07-27 16:31:51 +0300

    Add more tests

--- src/test/java/com/github/zeroone3010/yahueapi/HueTest.java
+++ src/test/java/com/github/zeroone3010/yahueapi/HueTest.java
@@ -14,6 +14,8 @@ import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class HueTest {
   private static final String API_KEY = "abcd1234";
@@ -31,15 +33,43 @@ class HueTest {
     wireMockServer.stop();
   }
 
-  @Test
-  void testInitialLoading() throws IOException {
+  private Hue createHueAndInitializeMockServer() throws IOException {
     final String hueRoot = readFile("hueRoot.json");
     wireMockServer.stubFor(get(API_BASE_PATH).willReturn(okJson(hueRoot)));
 
-    final Hue hue = new Hue("localhost:" + wireMockServer.port(), API_KEY);
-    hue.refresh();
+    return new Hue("localhost:" + wireMockServer.port(), API_KEY);
+  }
 
-    wireMockServer.verify(getRequestedFor(urlEqualTo(API_BASE_PATH)));
+  @Test
+  void testInitializationAndRefresh() throws IOException {
+    final Hue hue = createHueAndInitializeMockServer();
+    wireMockServer.verify(0, getRequestedFor(urlEqualTo(API_BASE_PATH)));
+
+    hue.refresh();
+    wireMockServer.verify(1, getRequestedFor(urlEqualTo(API_BASE_PATH)));
+
+    hue.refresh();
+    wireMockServer.verify(2, getRequestedFor(urlEqualTo(API_BASE_PATH)));
+  }
+
+  @Test
+  void testGetRooms() throws IOException {
+    final Hue hue = createHueAndInitializeMockServer();
+
+    assertEquals(2, hue.getRooms().size());
+    hue.getRooms();
+    hue.getRooms();
+    wireMockServer.verify(1, getRequestedFor(urlEqualTo(API_BASE_PATH)));
+  }
+
+  @Test
+  void testGetRoomByName() throws IOException {
+    final Hue hue = createHueAndInitializeMockServer();
+
+    assertEquals(2, hue.getRoomByName("Living room").get().getLights().size());
+    assertEquals(1, hue.getRoomByName("Bedroom").get().getLights().size());
+    assertFalse(hue.getRoomByName("No such room").isPresent());
+    wireMockServer.verify(1, getRequestedFor(urlEqualTo(API_BASE_PATH)));
   }
 
   private String readFile(final String fileName) throws IOException {
