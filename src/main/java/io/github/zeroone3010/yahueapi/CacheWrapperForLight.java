f042498847c26e4bda2f3f721965f5bbdf8b42e8
Ville Saalo <ville.saalo@iki.fi>
2019-07-07 00:46:23 +0300

    CacheWrapperForLight does not need to be public

--- src/main/java/io/github/zeroone3010/yahueapi/CacheWrapperForLight.java
+++ src/main/java/io/github/zeroone3010/yahueapi/CacheWrapperForLight.java
@@ -6,14 +6,14 @@ import io.github.zeroone3010.yahueapi.domain.LightState;
 
 import java.net.URL;
 
-public class CacheWrapperForLight extends LightImpl {
+final class CacheWrapperForLight extends LightImpl {
   private final Hue hue;
 
-  public CacheWrapperForLight(final Hue hue,
-                              final String id,
-                              final LightDto light,
-                              final URL url,
-                              final ObjectMapper objectMapper) {
+  CacheWrapperForLight(final Hue hue,
+                       final String id,
+                       final LightDto light,
+                       final URL url,
+                       final ObjectMapper objectMapper) {
     super(id, light, url, objectMapper);
     this.hue = hue;
   }
