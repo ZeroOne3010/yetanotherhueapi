9fbbace8cbfef0cde14ea27d9c83905f443e94fc
Ville Saalo <ville.saalo@iki.fi>
2020-12-03 16:33:37 +0200

    Remove unused imports

--- src/main/java/io/github/zeroone3010/yahueapi/Hue.java
+++ src/main/java/io/github/zeroone3010/yahueapi/Hue.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -23,7 +22,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
