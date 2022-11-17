cf67362f23f96a9e4d7c7bf745c833879941ad20
Ville Saalo <ville.saalo@iki.fi>
2022-11-17 16:09:24 +0200

    Fix GroupFactory to correctly provide lights for zones

--- src/main/java/io/github/zeroone3010/yahueapi/v2/GroupFactory.java
+++ src/main/java/io/github/zeroone3010/yahueapi/v2/GroupFactory.java
@@ -6,6 +6,8 @@ import io.github.zeroone3010.yahueapi.v2.domain.ResourceIdentifier;
 
 import java.net.URL;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -22,16 +24,25 @@ final class GroupFactory {
 
   public GroupImpl buildGroup(final GroupResource groupResource,
                               final URL resourceUrl) {
-    final Supplier<Collection<Light>> lightProvider = () ->
-        groupResource.getChildren().stream()
-            .filter(r -> r.getResourceType() == DEVICE)
-            .map(ResourceIdentifier::getResourceId)
-            .map(hue::getResource)
-            .map(r -> (DeviceResource) r)
-            .flatMap(r -> r.getServices().stream())
-            .filter(s -> s.getResourceType() == LIGHT)
-            .map(light -> hue.getLights().get(light.getResourceId()))
-            .collect(Collectors.toSet());
+    final Supplier<Collection<Light>> lightProvider = () -> {
+      final Set<Light> deviceLights = groupResource.getChildren().stream()
+          .filter(r -> r.getResourceType() == DEVICE)
+          .map(ResourceIdentifier::getResourceId)
+          .map(hue::getResource)
+          .map(r -> (DeviceResource) r)
+          .flatMap(r -> r.getServices().stream())
+          .filter(s -> s.getResourceType() == LIGHT)
+          .map(light -> hue.getLights().get(light.getResourceId()))
+          .collect(Collectors.toSet());
+      final Set<Light> childLights = groupResource.getChildren().stream()
+          .filter(r -> r.getResourceType() == LIGHT)
+          .map(ResourceIdentifier::getResourceId)
+          .map(id -> hue.getLights().get(id))
+          .collect(Collectors.toSet());
+      final Set<Light> result = new HashSet<>(deviceLights);
+      result.addAll(childLights);
+      return result;
+    };
     return new GroupImpl(groupResource.getId(),
         groupResource.getType(),
         groupResource.getMetadata().getName(),
